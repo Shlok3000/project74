8508b5ba76e09ab96ffe5c31fc8b824da96cd9d7
Shlok3000 <71341641+Shlok3000@users.noreply.github.com>
2021-06-26 14:53:25 -0400

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { SearchBar } from 'react-native-elements'
+
+export default class ReadStoryScreen extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            search: ''
+        }
+    }
+
+    retriveStories = async (search) => {
+        this.setState({ search });
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Read some stories</Text>
+                <SearchBar
+                    placeholder="Type here"
+                    onChangeText={this.retriveStories}
+                    value={search}
+                />
+            </View>
+        )
+    }
+}
---
+++ WriteStoryScreen.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { SearchBar } from 'react-native-elements'
+
+export default class WriteStoryScreen extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            search: ''
+        }
+    }
+
+    SearchFilterFunction = async (search) => {
+        this.setState({ search })
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Write some stories</Text>
+                <SearchBar
+                    placeholder="Type here"
+                    onChangeText={this.SearchFilterFunction}
+                    value={search}
+                />
+            </View>
+        )
+    }
+}
